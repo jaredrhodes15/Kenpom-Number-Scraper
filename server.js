25f4a88dc9b126eca50898d169c68d5d332babfd
Jared Rhodes <jaredrhodes15@gmail.com>
2019-04-26 18:49:00 -0600

    rebasing

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const logger = require("morgan");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+
+const PORT = process.env.PORT || 3000;
+// Configure middleware
+// Initialize Express
+const app = express();
+
+const mongoose = require("mongoose");
+
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
+// Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, () => {
+  console.log("App running on port " + PORT + "!");
+});
+
+module.exports = app;
